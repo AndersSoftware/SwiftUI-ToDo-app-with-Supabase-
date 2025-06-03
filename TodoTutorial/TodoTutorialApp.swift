8e8c22fd3c4dc7c949277a45ba786311c1c1e86f
AndersSoftware <155743530+AndersSoftware@users.noreply.github.com>
2025-06-03 13:21:48 +0200

    Init

---
+++ TodoTutorial/TodoTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoTutorialApp.swift
+//  TodoTutorial
+//
+//  Created by Anders Jensen on 25/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoTutorial/Viewmodels/TodoListViewmodel.swift
@@ -0,0 +1,7 @@
+//
+//  TodoListViewmodel.swift
+//  TodoTutorial
+//
+//  Created by Anders Jensen on 25/05/2025.
+//
+
